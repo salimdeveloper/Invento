4ef1405e8de930ede77064bede8e1ba1312a730f
Salim Alam <salimalamdeveloper@gmail.com>
2016-09-30 03:27:45 +0530

    EntityConfiguration FOr PartsPurchaseRecords

---
+++ Strado.InVento/Persistence/EntityConfigurations/PartsPurchaseRecordsConfigurations.cs
@@ -0,0 +1,24 @@
+﻿using Strado.InVento.Core.Models;
+using System.Data.Entity.ModelConfiguration;
+
+namespace Strado.InVento.Persistence.EntityConfigurations
+{
+    internal class PartsPurchaseRecordsConfigurations : EntityTypeConfiguration<PartsPurchaseRecords>
+    {
+        public PartsPurchaseRecordsConfigurations()
+        {
+
+            Property(p => p.PartsId)
+                .IsRequired();
+            Property(p => p.PurchaseDate)
+                .IsRequired();
+            Property(p => p.PurchaseQty)
+                .IsRequired();
+            Property(p => p.SuppliersId)
+                .IsRequired();
+            Property(p => p.PurchasePrice)
+                .IsRequired();
+
+        }
+    }
+}
