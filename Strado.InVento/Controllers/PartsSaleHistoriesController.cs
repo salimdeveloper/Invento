8c30fd17df29ea38c9e510d7c1d9aeebc31f700b
Salim Alam <salimalamdeveloper@gmail.com>
2016-09-30 03:12:38 +0530

    Patrs sales history and inventory updated

--- Strado.InVento/Controllers/PartsSaleHistoriesController.cs
+++ Strado.InVento/Controllers/PartsSaleHistoriesController.cs
@@ -1,5 +1,6 @@
 ﻿using Strado.InVento.Core.Interfaces;
 using Strado.InVento.Core.Models;
+using Strado.InVento.Core.ViewModels;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -25,13 +26,23 @@ namespace Strado.InVento.Controllers
         [Authorize]
         public ActionResult Sales()
         {
-            return View();
+            var _partsSalesHistory = new PartsSaleHistoryViewModel {
+                
+                Parts = _unitOfWork.Parts.GetAllParts()
+            };
+            return View(_partsSalesHistory);
         }
         [Authorize]
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Sales(PartsSaleHistory model)
+        public ActionResult Sales(PartsSaleHistoryViewModel model)
         {
+            if (!ModelState.IsValid)
+            {
+                
+                model.Parts = _unitOfWork.Parts.GetAllParts();
+                return View(model);
+            }
             var _partsSaleHistory = new PartsSaleHistory
             {
                 PartsId = model.PartsId,
@@ -40,6 +51,16 @@ namespace Strado.InVento.Controllers
                 QtyWithdrawn=model.QtyWithdrawn,
                 SoldPrice=model.SoldPrice
             };
+             
+            _unitOfWork.PartsSaleHistories.AddPartWithdrawl(_partsSaleHistory);
+            var _inventory = _unitOfWork.Inventory.GetInventoryByPartsId(model.PartsId);
+            if (_inventory != null)
+            {
+                _inventory.DeleteStockQuantity(model.PartsId, model.QtyWithdrawn, DateTime.Now);
+            }
+            _unitOfWork.Complete();
+          
+
             return RedirectToAction("PartsSalesList", "PartsSaleHistories");
         }
     }
