1eb3dcaa45d677bd4be09b3d20f13bd722580e09
Salim Alam <salimalamdeveloper@gmail.com>
2016-09-30 03:26:24 +0530

    Created The Inventory Repo

---
+++ Strado.InVento/Persistence/Repositories/InventoryRepository.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Strado.InVento.Core.Interfaces;
+using Strado.InVento.Core.Models;
+using Strado.InVento.Persistence.Data;
+using System.Linq;
+
+namespace Strado.InVento.Persistence.Repositories
+{
+    internal class InventoryRepository : IInventoryRepository
+    {
+        private readonly ApplicationDbContext _context;
+
+        public InventoryRepository(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public void AddInventory(Inventory inventory)
+        {
+            _context.Inventory.Add(inventory);
+        }
+
+        public Inventory GetInventoryByPartsId(int partsId)
+        {
+            return _context.Inventory.SingleOrDefault(i => i.PartsId == partsId);
+        }
+
+        public void UpdateInventory(Inventory _inventory)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
