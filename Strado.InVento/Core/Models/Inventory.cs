5771bea0f257f1af19bf8f3cf593e7dcf93ddf5d
Salim Alam <salimalamdeveloper@gmail.com>
2016-09-30 03:14:13 +0530

    Parts Purchase Record Model Created

--- Strado.InVento/Core/Models/Inventory.cs
+++ Strado.InVento/Core/Models/Inventory.cs
@@ -13,24 +13,20 @@ namespace Strado.InVento.Core.Models
         public int QtyInStock { get; set; }
         public DateTime LastUpdated { get; set; }
 
-        /// <summary>
-        /// Adds New Items Inventory
-        /// </summary>
-        /// <param name="_partId"></param>
-        /// <param name="_qtyToAdd"></param>
-        /// <param name="_dateTime"></param>
-        public void AddInventory(int _partId,int _qtyToAdd,DateTime _dateTime)
-        {
-            this.PartsId = _partId;
-            this.QtyInStock = _qtyToAdd;
-            this.LastUpdated = _dateTime;
-        }
+     
 
         public void DeleteStockQuantity(int _partId, int _qty, DateTime _dateTime)
         {
             if (this.PartsId != _partId)
                 throw new ArgumentException(nameof(PartsId));
             this.QtyInStock = QtyInStock - _qty;
+            this.LastUpdated = _dateTime;
+        }
+        public void AddStockQuantity(int _partId, int _qty, DateTime _dateTime)
+        {
+            if (this.PartsId != _partId)
+                throw new ArgumentException(nameof(PartsId));
+            this.QtyInStock = QtyInStock + _qty;
             this.LastUpdated = _dateTime;
         }
     }
---
+++ Strado.InVento/Core/Models/PartsPurchaseRecords.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Strado.InVento.Core.Models
+{
+    public class PartsPurchaseRecords
+    {
+        public int Id { get; set; }
+        public int PartsId { get; set; }
+        public Parts Parts { get; set; }
+        public double PurchasePrice { get; set; }
+        public int PurchaseQty { get; set; }
+        public DateTime PurchaseDate { get; set; }
+        public int SuppliersId { get; set; }
+        public Suppliers Suppliers { get; set; }
+
+    }
+}
