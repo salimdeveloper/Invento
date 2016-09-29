a87cdb55e145e9d364dcf4dbd8e69fc0806e54fc
Salim Alam <salimalamdeveloper@gmail.com>
2016-09-30 03:16:55 +0530

    Parts SaleHistory Viw Model Added

---
+++ Strado.InVento/Core/ViewModels/PartsSaleHistoryViewModel.cs
@@ -0,0 +1,19 @@
+﻿using Strado.InVento.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Strado.InVento.Core.ViewModels
+{
+    public class PartsSaleHistoryViewModel
+    {
+        public int Id { get; set; }
+        public int PartsId { get; set; }
+        public IEnumerable<Parts> Parts { get; set; }
+        public DateTime SoldDate { get; set; }
+        public double SoldPrice { get; set; }
+        public int QtyWithdrawn { get; set; }
+        public PartWithdrawlReason WithdrawlReason { get; set; }
+    }
+}
